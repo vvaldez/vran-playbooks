5dc29d41499fc1d96d537aca9449ffd18a68194c
Homero Pawlowski <hpawlows@redhat.com>
2019-10-09 01:33:38 -0400

    generate local templates works. added helper script to generate all envs at once

---
+++ generate-all-envs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+paths='
+blue/hosts-test
+green/hosts-dev
+green/hosts-test
+red/hosts-prod
+red/hosts-test'
+args=''
+
+if [[ ! -z ${1} && ${1} == 'quick' ]]; then
+  args='-e template_directory_quick_mode=true'
+fi
+
+for path in ${paths}; do
+  ansible-playbook -i ../ansible-inventory/${path} pb-generate-templates-locally.yml ${args}
+done
