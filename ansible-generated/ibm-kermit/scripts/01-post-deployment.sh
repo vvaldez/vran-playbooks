9d30761705103c1174c9b54f2d27fb32724419fe
Homero Pawlowski <hpawlows@redhat.com>
2019-11-06 23:29:12 -0500

    added vnc_keymap, provider-network steps

---
+++ ansible-generated/ibm-kermit/scripts/01-post-deployment.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+source ~/overcloudrc
+
+# Create provider networks
+
+openstack \
+  network \
+  create provider_network \
+  --provider-physical-network datacentre \
+  --provider-network-type flat \
+  --external \
+  --share
+
+openstack \
+  subnet \
+  create \
+  provider-subnet \
+  --network provider_network \
+  --dhcp \
+  --allocation-pool start=150.238.2.211,end=150.238.2.216 \
+  --gateway 150.238.2.193 \
+  --subnet-range 150.238.2.192/27
+
+# Create images
+wget http://download.cirros-cloud.net/0.4.0/cirros-0.4.0-x86_64-disk.img -O /home/stack/files/cirros-0.4.0-x86_64-disk.img
+
+openstack \
+  image \
+  create \
+  --disk-format qcow2 \
+  --container-format bare \
+  --file files/cirros-0.4.0-x86_64-disk.img \
+  cirros-0.4.0
+
+openstack \
+  flavor \
+  create \
+  --vcpus 1 \
+  --ram 128 \
+  m1.micro
--- ansible-generated/ibm-kermit/scripts/deploy.sh
+++ ansible-generated/ibm-kermit/scripts/deploy.sh
@@ -7,15 +7,15 @@ openstack overcloud deploy --templates \
 -r $CNF/roles_data.yaml \
 -n $CNF/network_data.yaml \
 -e $THT/environments/ceph-ansible/ceph-ansible-external.yaml \
--e $CNF/network-config.yaml \
--e $CNF/environments/network-isolation.yaml \
+-e $THT/environments/disable-telemetry.yaml \
 -e $CNF/environments/network-environment.yaml \
--e $CNF/overcloud-images.yaml \
--e $CNF/node-config.yaml \
--e $CNF/storage-environment.yaml \
+-e $CNF/environments/network-isolation.yaml \
 -e $CNF/ips-from-pool-all.yaml \
+-e $CNF/network-config.yaml \
+-e $CNF/node-config.yaml \
 -e $CNF/octavia.yaml \
--e $CNF/service_net_environment.yaml
+-e $CNF/overcloud-images.yaml \
+-e $CNF/service_net_environment.yaml \
+-e $CNF/storage-environment.yaml
 
-# -e $THT/environments/disable-telemetry.yaml \
 # -e $CNF/inject-trust-anchor.yaml \
