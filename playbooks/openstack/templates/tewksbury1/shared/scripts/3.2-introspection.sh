5075b6daf57e91ce4fdfaf1bcfc057cf077fce80
Homero Pawlowski <hpawlows@redhat.com>
2020-09-03 10:46:43 -0700

    move input templates to ansible-playbooks to support tower usage

---
+++ playbooks/openstack/templates/tewksbury1/shared/scripts/3.2-introspection.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# This script should be run as the stack user on the undercloud
+
+# instackenv.yaml should contain all hosts in the deployment.
+# In the OpenStack documentation, this file is called instackenv.json
+
+# [ -f /home/stack/instackenv.yaml ] || echo "instackenv.yaml not found" && exit 1
+source /home/stack/stackrc
+
+
+#######################
+# Import Ironic Nodes #
+#######################
+openstack overcloud node import ~/dell-lab/stack-home/instackenv.yaml
+
+# now we have to map the neutron ports individual servers to their leaf
+for NODE in controller1 controller2 controller3 compute-1;do
+PORT=$(openstack baremetal port list --node $NODE -f value -c UUID)
+openstack baremetal port set --physical-network ctlplane $PORT
+done
+
+for NODE in edge1vdu-1 edge1-1;do
+PORT=$(openstack baremetal port list --node $NODE -f value -c UUID)
+openstack baremetal port set --physical-network edge1 $PORT
+done
+
+for NODE in edge2vdu-1 edge2-1;do
+PORT=$(openstack baremetal port list --node $NODE -f value -c UUID)
+openstack baremetal port set --physical-network edge2 $PORT
+done
+
+# Introspect, clean and set to available
+openstack overcloud node introspect --all-manageable --provide
