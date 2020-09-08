c5969f754665ed5e8342aa0d723ef2ffc6d3e1f3
Homero Pawlowski <hpawlows@redhat.com>
2020-09-07 23:32:30 -0700

    refactor template upload and generation playbooks; refactor loop_index variable in template generation to more meaningful name

--- playbooks/openstack/templates/tewksbury1/shared/scripts/_map_neutron_ports.sh
+++ playbooks/openstack/templates/tewksbury1/shared/scripts/_map_neutron_ports.sh
@@ -5,13 +5,13 @@ set -x
 source /home/stack/stackrc
 
 {% for site in sites %}
-{%   set loop_index = loop.index0 %}
+{%   set index = loop.index0 %}
 # {{ site.name_lower }}
 {%   for role in site.roles %}
 {%     for n in range(role.count) %}
 {%       set name="%s-%s-%s"|format(role.type.name_lower, site.name_lower, n + 1) %}
 PORT=$(openstack baremetal port list --node {{ name }} -f value -c UUID)
-{%       if loop_index == 0 %}
+{%       if index == 0 %}
 openstack baremetal port set --physical-network ctlplane $PORT
 {%       else %}
 openstack baremetal port set --physical-network {{ site.name_lower }} $PORT
